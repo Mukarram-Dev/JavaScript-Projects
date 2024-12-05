9dc933f34ce54465b0f6c23c2c2af692264565ce
Mukarram Ali <bossmukaram@gmail.com>
2024-12-06 00:41:20 +0500

    find ratio problem code done

--- find_ratio_problem/main.js
+++ find_ratio_problem/main.js
@@ -6,18 +6,40 @@ const rl = readline.createInterface({
 });
 
 function plusMinus(arr) {
-  console.log(arr);
+  let positiveCount = 0;
+  let negativeCouunt = 0;
+  let zeroCount = 0;
+
+  arr.forEach((element) => {
+    if (element == 0) {
+      zeroCount++;
+    } else if (element < 0) {
+      negativeCouunt++;
+    } else {
+      positiveCount++;
+    }
+  });
+  const posRatio = positiveCount/arr.length;
+  const negRatio = negativeCouunt/arr.length;
+  const zeroRatio = zeroCount/arr.length;
+
+  console.log(`${posRatio.toPrecision(6)}\n${negRatio.toPrecision(6)}\n${zeroRatio.toPrecision(6)}`);
+
 }
 
 async function main() {
   let arr = [];
-  
-  const n =await rl.question("Enter the size of array: ");
+
+  const n = await rl.question("Enter the size of array: ");
 
   for (let i = 0; i < n; i++) {
-    let element = await rl.question("Enter the element for array " + i + 1+ ' : ');
-    arr.push(element);
+    let element = await rl.question(
+      "Enter the element for array " + (i + 1) + " : "
+    );
+    arr.push(parseInt(element));
   }
+
+  console.log(`Your array is this : ${arr}\n`);
 
   plusMinus(arr);
 }
